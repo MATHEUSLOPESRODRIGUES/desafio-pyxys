625b39964ab7367cb1eeae64d79ff318664d04df
Matheus Lopes <mateuslopesro14@gmail.com>
2025-07-20 17:28:54 -0300

    Adiciona o index.php carregando o header e o footer

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <section style="width: 100vw; height:100vh">Section 1</section>
+    <section style="width: 100vw; height:100vh">Section 2</section>
+    <section style="width: 100vw; height:100vh">Section 3</section>
+    <section style="width: 100vw; height:100vh">Section 4</section>
+</div>
+
+<?php get_footer(); ?>
