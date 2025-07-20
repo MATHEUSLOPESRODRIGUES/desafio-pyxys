6337d5302567c3b22d3be7e1c5133877ddb2c7da
Matheus Lopes <mateuslopesro14@gmail.com>
2025-07-20 17:26:44 -0300

    Adiciona o footer.php com a estrutura html semântica do rodapé

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <section>
+        <img src="<?php echo get_template_directory_uri(); ?> /assets/img/pose-apresentando.png" alt="pose-apresentando"
+            width="500px" height="500px">
+
+        <form action="">
+            <div class="content">
+                <div class="form-text">
+                <p>vamos conversar</p>
+                <h2>Você, gestor de uma grande empresa: vai deixar sua marca por fora da nova realidade digital?</h2>
+                <p>Qual é a melhor pessoa e e-mail de sua empresa para que o consultor
+                    do combo <strong> Brand Persona PYXYS + Ilustraria</strong> entre em contato?</p>
+            </div>
+
+            <fieldset class="informations">
+                <input type="text" placeholder="Nome">
+                <input type="email" placeholder="E-mail empresarial">
+                <input type="text" placeholder="Cargo">
+            </fieldset>
+            <button>ENVIAR CONTATO</button>
+            </div>
+        </form>
+    </section>
+
+    <div class="sub-header">
+        <p>Brand Persona - 2022 - Todos os direitos reservados</p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
